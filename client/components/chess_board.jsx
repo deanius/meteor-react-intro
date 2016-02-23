550fd4ba7bdc04e7e926d09b3a45ee72f9296a87
Dean Radcliffe <dradcliffe@shutterstock.com>
2016-02-22 22:38:39 -0600

    implement moves. game, set, match.

--- client/components/chess_board.jsx
+++ client/components/chess_board.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ChessBoardJS from 'chessboardjs'
-
+import {movePiece} from '../../lib/chess.js'
 const containerId = 'chessboardjs-div'
 
 export default React.createClass({
-  componentDidUpdate() {
+  componentDidMount() {
+    let currentBoard = this.props.board;
     // be naughty and mutate the DOM after React
     ChessBoardJS(containerId, {
       draggable: true,
-      position: this.props.board
+      position: currentBoard,
+      onDrop: function(source, target) {
+        let newBoard = movePiece(currentBoard, source, target)
+        // optimistically update the local collection, let Meteor sync it
+        Boards.insert(newBoard)
+      }
     })
   },
   render() {
